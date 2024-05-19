08437c7967e422b8660e2a043d13ba085d433b83
Srinivas k <109860232+KolipakaSrinivas@users.noreply.github.com>
2024-05-19 19:48:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,6 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// /api/fileanalyse
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const {size,originalname,mimetype} = req.file;
+  res.json({name:originalname,type:mimetype,size:size});
+})
 
 
 
